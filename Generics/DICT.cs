a65a06e965614b4bd0c7066dde60c25e76e862b0
noobprogramme <31530225+noobprogramme@users.noreply.github.com>
2019-04-14 21:16:11 -0400

    Add files via upload

---
+++ Generics/DICT.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class DICT
+    {
+        static void Main()
+        {
+            Dictionary<int, string> datas = new Dictionary<int, string>()
+                                            {
+                                                {1,"One"},
+                                                {2, "Two"},
+                                                {3,"Three"}
+                                            };
+
+            for (int i = 0; i < datas.Count; i++)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", datas.Keys.ElementAt(i), datas[datas.Keys.ElementAt(i)]);
+            }
+            for (int i = 0; i < datas.Count; i++)//cannot get values 
+            {
+                Console.WriteLine(datas.Keys +" "+ datas.Values);
+            }
+
+            foreach (var i in datas)
+            {
+                Console.WriteLine(i.Key + "  " + i.Value);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class employee
+    {
+        public string name;
+        public string pos;
+        public int id;
+
+    }
+    class Program
+    {
+    //    public T ii { get; set; }
+    //    public V ss { get; set; }
+         
+    //    public  void operations(T Ii, V Ss) {
+    //        this.ii = Ii;
+    //        this.ss = Ss;
+
+    //    }
+
+
+        static void Main(string[] args)
+        {
+            List<employee> emp = new List<employee>();
+            employee c1 = new employee { id = 123, name = "hemanth", pos = "software" };
+            employee c2 = new employee { id = 231, name = "kumar", pos = "data " };
+            emp.Add(c1);
+            emp.Add(c2);
+            foreach(employee obj in emp)
+            {
+                Console.WriteLine(obj.pos+ "   "+obj.name);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
